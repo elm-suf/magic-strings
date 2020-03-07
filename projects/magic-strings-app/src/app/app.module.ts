3493920cfce706df8184bbbf80d2a067f2917eb9
Sofiane El Marhani <s.elmarhani.ext@teamsystem.com>
2020-03-07 03:16:05 +0100

    add build script

--- projects/magic-strings-app/src/app/app.module.ts
+++ projects/magic-strings-app/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MagicLibModule} from 'magic-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MagicLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
